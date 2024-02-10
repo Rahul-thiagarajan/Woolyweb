f001d46c09298619c80e175de1f9aede286eb9cc
K-Okid <agatheeran27@gmail.com>
2024-02-11 01:39:21 +0530

    last changes of the day

--- static/checkout.js
+++ static/checkout.js
@@ -6,25 +6,25 @@ $(".total").html(localStorage.productPrice);
 
 $(".delivery-option-input").on("click", function() {
     if (this.id == 1) {
-        $(".shipping-charge").html("$0.00");
+        $(".shipping-charge").html("Rs.0.00");
         $(".delivery-date").html("Delivery date: Tuesday, June 21");
     } else if (this.id == 2) {
-        $(".shipping-charge").html("$4.99");
+        $(".shipping-charge").html("Rs.4.99");
         $(".delivery-date").html("Delivery date: Wednesday, June 15");
     } else if (this.id == 3) {
-        $(".shipping-charge").html("$9.99");
+        $(".shipping-charge").html("Rs.9.99");
         $(".delivery-date").html("Delivery date: Monday, June 13");
     }
     initPrice();});
 initPrice();
 function initPrice() {
-    var total = parseFloat($(".total").html().slice(1));
-    var shippingCharge = parseFloat($(".shipping-charge").html().slice(1));
-    $(".total-before-tax").html("$" + (total + shippingCharge).toFixed(2));
-    var totalBeforeTax = parseFloat($(".total-before-tax").html().slice(1));
+    var total = parseFloat($(".total").html().slice(3));
+    var shippingCharge = parseFloat($(".shipping-charge").html().slice(3));
+    $(".total-before-tax").html("Rs." + (total + shippingCharge).toFixed(2));
+    var totalBeforeTax = parseFloat($(".total-before-tax").html().slice(3));
     var taxValue = (10/100) * totalBeforeTax;
-    $(".tax").html("$" + taxValue.toFixed(2));
-    var tax = parseFloat($(".tax").html().slice(1));
+    $(".tax").html("Rs." + taxValue.toFixed(2));
+    var tax = parseFloat($(".tax").html().slice(3));
     var orderTotalValue = totalBeforeTax + tax;
-    $(".order-total").html("$" + orderTotalValue.toFixed(2));
-    var orderTotal = parseFloat($(".order-total").html().slice(1));}
+    $(".order-total").html("Rs." + orderTotalValue.toFixed(2));
+    var orderTotal = parseFloat($(".order-total").html().slice(3));}
