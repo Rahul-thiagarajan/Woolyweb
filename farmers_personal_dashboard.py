89818c8059c113b4049a137615eb033a53fc2e0d
Sanjai solairaja <ssolairaja105@gmail.com>
2024-02-11 11:13:06 +0530

    created function personal details

--- farmers_personal_dashboard.py
+++ farmers_personal_dashboard.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
-
+import pymysql
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('personal_details_dashboard.html')
 
-# @app.route('/dashboard')
-# def dashboard():
-#     return render_template('dashboard.html')
+@app.route('/Personal_details')
+def dashboard():
+     obj7=pymysql.connect(host="localhost",user="root",database="woolyweb",password="abcd")
+     cur7=obj7.cursor()
+     return render_template('personal_detail_dashboard.html',value="pd")
 
 # @app.route('/help')
 # def help():
