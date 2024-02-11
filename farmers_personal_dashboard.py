1176aa5c6de135f1082b792eb113c8db3f24f43a
Rahulthiagarajan <rahulthiagarajan2004@gmail.com>
2024-02-11 17:05:55 +0530

    prepared personal details and processing page

--- farmers_personal_dashboard.py
+++ farmers_personal_dashboard.py
@@ -19,11 +19,27 @@ def personal_details():
 def processing_details():
     obj=pymysql.connect(host="localhost",user="root",password="abcd",database="woolyweb")
     cur=obj.cursor()
-    s="select processing from farmers where fid="+str(Farmerid)+";"
+    s="select processing_access from farmers where fid="+str(Farmerid)+";"
     cur.execute(s)
-    if(cur.fetchall()[0][0]=="n"):
+    if(cur.fetchall()[0][0].lower()=="n"):
         return render_template('personal_details_dashboard.html',value="pn",Fid=Farmerid)
     return render_template('personal_details_dashboard.html',value="py",Fid=Farmerid)
+
+@app.route('/Processing_details_change',methods=["POST","GET"])
+def processing_details_change():
+    obj=pymysql.connect(host="localhost",user="root",password="abcd",database="woolyweb")
+    cur=obj.cursor()
+    if(request.method=="POST"):
+        if('requesting_processing' in request.form and request.form['requesting_processing'] == 'y'):
+            a="Update farmers set processing_access='y' where fid="+str(Farmerid)+";"
+            cur.execute(a)
+            obj.commit()
+            return render_template('personal_details_dashboard.html',value="py",Fid=Farmerid)
+        else:
+            a="Update farmers set processing_access='n' where fid="+str(Farmerid)+";"
+            cur.execute(a)
+            obj.commit()
+            return render_template('personal_details_dashboard.html',value="pn",Fid=Farmerid)
 # @app.route('/help')
 # def help():
 #     return render_template('help.html')
