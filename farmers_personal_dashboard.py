f08dfd73c83e9131e9f345cbba1e62d744b88a4f
Rahulthiagarajan <rahulthiagarajan2004@gmail.com>
2024-02-13 13:34:31 +0530

    ..

--- farmers_personal_dashboard.py
+++ farmers_personal_dashboard.py
@@ -27,9 +27,9 @@ def personal_details():
 
 @app.route('/Processing_details_change',methods=["POST","GET"])
 def processing_details_change():
+    obj=pymysql.connect(host="localhost",user="root",password="abcd",database="woolyweb")
+    cur=obj.cursor()
     if request.method=="POST":
-        obj=pymysql.connect(host="localhost",user="root",password="abcd",database="woolyweb")
-        cur=obj.cursor()
         if('requesting_processing' in request.form and request.form['requesting_processing'] == 'y'):
             a="Update farmers set processing_access='y' where fid="+str(Farmerid)+";"
             cur.execute(a)
@@ -41,8 +41,6 @@ def processing_details_change():
             obj.commit()
             return render_template('personal_details_dashboard.html',value="pn",Fid=Farmerid)
     else:
-        obj=pymysql.connect(host="localhost",user="root",password="abcd",database="woolyweb")
-        cur=obj.cursor()
         s="select processing_access from farmers where fid="+str(Farmerid)+";"
         cur.execute(s)
         if(cur.fetchall()[0][0].lower()=="n"):
